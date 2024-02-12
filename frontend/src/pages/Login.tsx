1c8d22cd537c82433e13cadbfc483a4d167574cb
jeremdh62 <jdhaille@myges.fr>
2024-02-12 23:31:48 +0100

    Add AuthLayout and Login page components

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,75 @@
+import AuthLayout from '@/layouts/AuthLayout';
+
+export default function Login() {
+  function onSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+  }
+
+  return (
+    <AuthLayout>
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-white">
+          Sign in to your account
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form onSubmit={onSubmit} className="space-y-6" action="#" method="POST">
+          <div>
+            <label htmlFor="email" className="block dark:text-white text-sm font-medium leading-6 text-gray-900">
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="input input-bordered w-full"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label htmlFor="password" className="dark:text-white block text-sm font-medium leading-6 text-gray-900">
+                Password
+              </label>
+              <div className="text-sm">
+                <a href="#" className="font-semibold text-success hover:text-accent">
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="input input-bordered w-full"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button className="btn btn-block btn-success text-white">Sign in</button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Not a member?{' '}
+          <a href="/signup" className="font-semibold leading-6 text-success hover:text-accent">
+            Sign up now
+          </a>
+        </p>
+      </div>
+    </AuthLayout>
+  );
+}
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 import { getIconCollections, iconsPlugin } from '@egoist/tailwindcss-icons';
+import pluginDaisyui from 'daisyui';
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -40,5 +41,6 @@ export default {
     iconsPlugin({
       collections: getIconCollections(['solar', 'tabler']),
     }),
+    pluginDaisyui,
   ],
 };
