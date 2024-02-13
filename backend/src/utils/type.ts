9e0477482e0bc4df75c22b31c6488d9dad899ba0
Matteo Delandhuy <matteodelandhuypro@outlook.fr>
2024-02-13 13:37:27 +0100

    better type done

--- backend/src/utils/type.ts
+++ backend/src/utils/type.ts
@@ -4,3 +4,6 @@ import mongoose, { Document, Model, Schema } from 'mongoose';
 type ExtractDoc<T> = T extends Model<infer U> ? U : never;
 type ExtractI<T> = Pick<T, Exclude<keyof T, keyof Document>>;
 export type ExtractInterface<T> = ExtractI<ExtractDoc<T>> & { _id: ObjectId };
+export type ExtractInterfaceWithoutId<T> = Omit<ExtractInterface<T>, '_id'>;
+
+export type PartialWithoutId<T> = Partial<Omit<T, '_id'>>;
