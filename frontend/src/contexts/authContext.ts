ef83be598e49f535a261c93fd18b223536bc768b
Sonny <sonny.luscap@gmail.com>
2024-02-13 13:10:04 +0100

    AuthContext

--- frontend/src/contexts/authContext.ts
+++ frontend/src/contexts/authContext.ts
@@ -2,3 +2,4 @@ import { createContext } from 'react';
 import AuthContextType from '@/types/AuthContextType';
 
 export const AuthContext = createContext<AuthContextType>({ auth: null, setAuth: () => {} });
+ // 
